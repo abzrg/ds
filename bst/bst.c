7a59f4ba49451b8badbaaf213d3eeca898595ce2
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2023-11-17 02:21:00 +0330

    bst: add del

--- bst/bst.c
+++ bst/bst.c
@@ -26,8 +26,11 @@ typedef bnode_t** bsst_t;
 static bnode_t* bnode_create(T value);
 static ssize_t bst_insertleaf(bsst_t subtree, T value);
 static ssize_t bst_insertsubtree(bsst_t subtree, T value);
+static bnode_t* bst_maxsubtree(bsst_t subtree);
+static ssize_t bst_delsubtree(bsst_t subtree, T value);
 
 ssize_t bst_insert(bst_t* tree, T value);
+ssize_t bst_del(bst_t* tree, T value);
 
 
 /* - Implementation - */
@@ -130,6 +133,112 @@ ssize_t bst_insert(bst_t* tree, T value)
     return ok;
 }
 
+/*
+    Finds the maximum node in the given subtree
+
+    @subtree: the subtree node to which the max node reside (which is the right
+    most node)
+
+    Returns: ptr to the max node
+*/
+static bnode_t* bst_maxsubtree(bsst_t subtree)
+{
+    assert(subtree != NULL && "NULL subtree has been passed.");
+
+    bsst_t tmp = subtree;
+    while (*tmp != NULL && (*tmp)->right != NULL) {
+        tmp = &((*tmp)->right);
+    }
+
+    return *tmp;
+}
+
+/*
+    Removes a node in a subtree that has a certain value
+
+    @subtree: the subtree node in which the value to be removed may reside
+    @value: the node with this value will be removed in the subtree (if exists)
+
+    Returns: 1 on success, 0 on failure
+*/
+static ssize_t bst_delsubtree(bsst_t subtree, T value)
+{
+    assert(subtree != NULL && "NULL subtree passed.");
+
+    // whoops didn't found it
+    if (*subtree == NULL) {
+        return 0L;
+    }
+
+    /// TODO probably more checks
+
+    if (value > (*subtree)->value) {
+        return bst_delsubtree(&((*subtree)->right), value);
+    } else if (value < (*subtree)->value) {
+        return bst_delsubtree(&((*subtree)->left), value);
+    } else {
+        // If you found it on a leaf
+        if ((*subtree)->left == NULL && (*subtree)->right == NULL) {
+            free(*subtree);
+            *subtree = NULL;
+            return 1L;
+        }
+
+        // If left node is empty, then steal right branch' root (move)
+        if ((*subtree)->left == NULL) {
+            bnode_t* rst = (*subtree)->right;
+            (*subtree)->value = rst->value;
+            (*subtree)->left = rst->left;
+            (*subtree)->right = rst->right;
+            free(rst);
+            return 1L;
+        }
+
+        /// TODO eliminate code duplication -> move function()
+
+        // If right node is empty, then steal left branch's root (move)
+        if ((*subtree)->right == NULL) {
+            bnode_t* lst = (*subtree)->left;
+            (*subtree)->value = lst->value;
+            (*subtree)->left = lst->left;
+            (*subtree)->right = lst->right;
+            free(lst);
+            return 1L;
+        }
+
+        // Replace to-be-deleted with the max node in left subtree
+        bnode_t* max_left = bst_maxsubtree(&((*subtree)->left));
+        (*subtree)->value = max_left->value;
+        (void)bst_delsubtree(&((*subtree)->left), max_left->value);
+
+        return 1L;
+    }
+}
+
+
+/*
+    Removes a node in a tree
+
+    @tree: the address of the tree
+    @value: the value of the node to be removed
+
+    Returns: 1 on success, 0 on failure
+*/
+ssize_t bst_del(bst_t* tree, T value)
+{
+    // No node in the tree
+    if (!tree->root) {
+        return 0L;
+    }
+
+    ssize_t ok = bst_delsubtree(&(tree->root), value);
+
+    if (ok) {
+        tree->length--;
+    }
+
+    return ok;
+}
 
 /* - Test - */
 
@@ -137,13 +246,54 @@ ssize_t bst_insert(bst_t* tree, T value)
 int main(void)
 {
     bst_t bst = { 0 };
-    bst_insert(&bst, 20);
-    bst_insert(&bst, 10);
-    bst_insert(&bst, 30);
 
-    printf("%d\n", bst.root->value);
-    printf("%d\n", bst.root->left->value);
-    printf("%d\n", bst.root->right->value);
+    /* Insertion */
+    bst_insert(&bst, 20);
+        bst_insert(&bst, 10);
+            bst_insert(&bst, 5);
+                bst_insert(&bst, 2);
+                bst_insert(&bst, 7);
+            bst_insert(&bst, 15);
+                bst_insert(&bst, 13);
+                bst_insert(&bst, 17);
+        bst_insert(&bst, 30);
+            bst_insert(&bst, 25);
+                bst_insert(&bst, 23);
+                bst_insert(&bst, 28);
+            bst_insert(&bst, 35);
+                bst_insert(&bst, 33);
+                bst_insert(&bst, 38);
+
+
+    /* Test printf */
+    printf("After insertion:\n");
+    printf("bst.root->value: %d\n", bst.root->value);
+    printf("bst.root->left->value: %d\n", bst.root->left->value);
+    printf("bst.root->right->value: %d\n", bst.root->right->value);
+    printf("size: %zu\n", bst.length);
+
+    /* Deletion */
+    bst_del(&bst, 20);
+        bst_del(&bst, 10);
+            bst_del(&bst, 5);
+                bst_del(&bst, 2);
+                bst_del(&bst, 7);
+            bst_del(&bst, 15);
+                bst_del(&bst, 13);
+                bst_del(&bst, 17);
+        bst_del(&bst, 30);
+            bst_del(&bst, 25);
+                bst_del(&bst, 23);
+                bst_del(&bst, 28);
+            bst_del(&bst, 35);
+                bst_del(&bst, 33);
+                bst_del(&bst, 38);
+
+
+    // printf("%d\n", bst.root->value);
+    printf("\nAfter deletion:\n");
+    printf("size: %zu\n", bst.length);
+
     return 0;
 }
 
